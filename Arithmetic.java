3662f6b968761f1168f8031648a3e7f1443c80ac
talha959 <i.talha.rajpot@gmail.com>
2025-02-08 16:52:02 +0500

    second commit

--- Arithmetic.java
+++ Arithmetic.java
@@ -14,16 +14,22 @@ public class Arithmetic {
         double difference = num1 - num2;
         double product = num1 * num2;
         double quotient = (num2 != 0) ? (num1 / num2) : Double.NaN;
+        double modulus = num1 % num2;
+        double exponentiation = Math.pow(num1, num2);
         
+        // Display results
         System.out.println("Addition: " + sum);
         System.out.println("Subtraction: " + difference);
         System.out.println("Multiplication: " + product);
         
         if (num2 != 0) {
             System.out.println("Division: " + quotient);
+            System.out.println("Modulus: " + modulus);
         } else {
             System.out.println("Division by zero is not allowed.");
         }
+        
+        System.out.println("Exponentiation: " + exponentiation);
         
         scanner.close();
     }
